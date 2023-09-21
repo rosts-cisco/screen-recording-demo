264f6a6d4e2727dfaa02b644f7b135f9c0ba5e53
rosts-cisco <98326687+rosts-cisco@users.noreply.github.com>
2023-09-21 19:54:35 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/screen-recording-demo/',
   server: {
     port: 5555,
-    // hmr: false,
   },
 });
