9a5a6237685b700e651eeece0c52fb4ccb47ac1e
rosts-cisco <98326687+rosts-cisco@users.noreply.github.com>
2025-01-22 11:31:28 +0400

    upd

--- src/message/AppMessage.tsx
+++ src/message/AppMessage.tsx
@@ -8,6 +8,13 @@ export function AppMessage() {
 
   return (
     <main className='pt-2 pb-12'>
+      <iframe
+        src='https://localhost:4444/share/embed/7f7e2fef-f7b4-4bc6-b41d-a80e21a85bf1'
+        width='540px'
+        height='304px'
+        loading='lazy'
+        allow='fullscreen *;autoplay *;'
+      />
       <div className='flex gap-3 mt-4 justify-center'>
         <button
           className='w-40 bg-slate-300 hover:bg-slate-400 hover:disabled:bg-slate-300 disabled:opacity-25'
