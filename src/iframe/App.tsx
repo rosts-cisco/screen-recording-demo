cff144ee7886441c42496e6efe18126151d93232
rosts-cisco <98326687+rosts-cisco@users.noreply.github.com>
2025-04-03 14:29:41 +0200

    upd

--- src/iframe/App.tsx
+++ src/iframe/App.tsx
@@ -4,7 +4,7 @@ export function App() {
       <div>
         video
         <iframe
-          src='https://localhost:4444/share/embed/7f7e2fef-f7b4-4bc6-b41d-a80e21a85bf1'
+          src='https://integration.vidcast.io/share/embed/7f7e2fef-f7b4-4bc6-b41d-a80e21a85bf1'
           width='540px'
           height='304px'
           loading='lazy'
@@ -15,7 +15,7 @@ export function App() {
       <div>
         playlist
         <iframe
-          src='https://localhost:4444/playlists/embed/c1d47c89-4274-4817-b84e-33ba9f8c9e81'
+          src='https://integration.vidcast.io/playlists/embed/c1d47c89-4274-4817-b84e-33ba9f8c9e81'
           width='540px'
           height='304px'
           loading='lazy'
@@ -26,9 +26,9 @@ export function App() {
       <div>
         page
         <iframe
-          src='https://localhost:4444/pages/embed/f1db5c0c-e1e0-449e-a497-f0e721e3d311'
+          src='https://integration.vidcast.io/pages/embed/f1db5c0c-e1e0-449e-a497-f0e721e3d311'
           width='540px'
-          height='304px'
+          height='540px'
           title='April 05, 2023 at 8:49 PM'
           loading='lazy'
           allow='fullscreen *;autoplay *;'></iframe>
